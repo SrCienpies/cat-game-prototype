9c1d9798132b1a3b29726a058ba751227a456511
SrCienpies <diego.detomas96@gmail.com>
2022-11-20 11:28:59 -0500

    Camera Conroller
    The camera controller targets the player and follows it, always keeping the isometric view.

--- Unity-CatGame-Prototype/Assets/Scripts/CameraController.cs
+++ Unity-CatGame-Prototype/Assets/Scripts/CameraController.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _target;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float _smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 _offset;
+
+    private void FixedUpdate()
     {
-        
+        Vector3 desiredPosition = _target.position + _offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+
+        transform.position = smoothPosition;
     }
 }
